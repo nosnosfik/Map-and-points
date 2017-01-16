d3ae1cfc318beb57238a21b55a4b5d6b5f732e9d
Nikita Taranov <nosfik@gmail.com>
2017-01-16 18:22:35 +0200

    Initial Commit

---
+++ Map-and-points/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map-and-points
+//
+//  Created by Nikita Taranov on 1/16/17.
+//  Copyright © 2017 Nikita Taranov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
